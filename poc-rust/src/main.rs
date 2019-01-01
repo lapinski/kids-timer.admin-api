9e58ee926394cd1c5e83e10b911b9bbc7000d9ae
Alex Lapinski <contact@alexlapinski.name>
2019-01-01 18:51:39 -0500

    WIP Working on Rust API POC

--- poc-rust/src/main.rs
+++ poc-rust/src/main.rs
@@ -1,4 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::json::Json;
 
 #[macro_use] extern crate rocket;
 
@@ -7,21 +10,44 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/user")]
-fn get_users() -> &'static str {
-    "Here are all of the users...."
+#[derive(Serialize, Deserialize)]
+struct User {
+    first_name: String,
+    last_name: String,
 }
 
-#[post("/user")]
-fn add_user() -> &'static str {
-    "Trust me, i created a user"
+#[get("/user/<id>", format = "json")]
+fn get_user(id: usize) -> Json<User> {
+    Json(User {
+        first_name: "Something".to_string(),
+        last_name: "XYZ".to_string()
+    })
+}
+
+#[get("/user")]
+fn get_users() -> Json<[User; 1]> {
+    let users: [User; 1] = [
+        User {
+            first_name: "aFirstName".to_string(),
+            last_name: "aLastName".to_string()
+        }
+    ];
+
+    /* TODO: Implement this */
+
+    return Json(users);
+}
+
+#[post("/user", data="<user>")]
+fn add_user(user: Json<User>) {
+    /* TODO: Implement this */
 }
 
 fn main() {
-    const baseUrl: &str = "/api/v1";
+    const BASE_URL: &str = "/api/v1";
     rocket::ignite()
-        .mount(baseUrl, routes![get_users])
-        .mount(baseUrl, routes![add_user])
-        .mount(baseUrl, routes![index])
+        .mount(BASE_URL, routes![get_users])
+        .mount(BASE_URL, routes![add_user])
+        .mount(BASE_URL, routes![index])
         .launch();
 }
